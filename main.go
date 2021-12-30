e974ac31ed61cf18a2a164e700e589a6d97fcf71
Naseredin Aramnejad <naseredin.aramnejad@gmail.com>
2021-12-30 10:38:42 +0300

    comment the code

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+/* blackbird logs in to the linux machines in parallel and will query their current
+ram, cpu and disk utilizations and provides log/mail notifications if configured.
+it can access the nodes directly from this machine or via a ssh gateway (tunnel).
+*/
 package main
 
 import (
@@ -11,41 +15,90 @@ import (
 )
 
 func main() {
+
+	//config.json is the file to be configured to determine the script execution steps, mail and sshgw addresses, worker quantity, etc.
 	configFileName := "config.json"
+
+	//configFilePath is used to change the config file's path to an os specific address.
 	configFilePath := filepath.Join("conf", configFileName)
+
+	//Config reads the json file and returns a struct which contains config key/value pairs.
 	Config := ioreader.ConfigLoader(configFilePath)
+
+	//Nodes contains the linux machines' credentials and addresses (Parsed from the csv file) to be able to monitor them.
+	//the file name is specified in the config.json file.
 	Nodes := ioreader.NodeLoader(filepath.Join("input", Config.InputFileName), Config.SshTunnel)
 
+	//cmds contains the linux bash commands to be executed on the linux machines.
 	cmds := []string{
-		`cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}'`, //-- CPU Query
-		`df -hP`,  //--------------------------------------------------------------------------------------------------------------------------------- Disk Query
-		`free -m`, //--------------------------------------------------------------------------------------------------------------------------------- RAM Query
+
+		//CPU Query.
+		`cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}'`,
+
+		//Disk Query.
+		`df -hP`,
+
+		//RAM Query.
+		`free -m`,
 	}
 
+	//nedata is a channel made from the struct which can contain the linux results per ne.
+	//if the query is successful, we expect to receive nedata channel from each node.
 	nedata := make(chan retriever.ResourceUtil, len(Nodes))
+
+	//errdata channel will be true in case data query fails from the node.
 	errdata := make(chan bool)
+
+	//wg manages the workers to be restricted to the specified value in the config.json file.
 	var wg sync.WaitGroup
 
+	//busyWorkers registers how many workers are currently busy to be used for keeping the worker quantity as specified in the config.json file.
 	busyWorkers := 0
 
-	for {
-		Config = ioreader.ConfigLoader(configFilePath)
-		qint, _ := strconv.ParseInt(Config.QueryInterval, 10, 16)
-		totalWorker, _ := strconv.ParseInt(Config.WorkerQuantity, 10, 64)
-		log.Printf("Total Workers: %v\n", totalWorker)
+	//################################################################################################################################################
 
+	//this for{} executes the node iteration based on the configured interval.
+	for {
+
+		//here we read and parse the config.json file again so we can change the parameters on the fly and during the execution.
+		Config = ioreader.ConfigLoader(configFilePath)
+
+		//qint is the query interval in second.
+		qint, _ := strconv.ParseInt(Config.QueryInterval, 10, 16)
+
+		//totalWorkers ==> how many concurrent query we can have.
+		totalWorkers, _ := strconv.ParseInt(Config.WorkerQuantity, 10, 64)
+
+		log.Printf("Total Workers: %v\n", totalWorkers)
+
+		//this for{} iterates through the nodes and invoked the retriever.NodeQuery() per node.
 		for _, ne := range Nodes {
-			if int64(busyWorkers) >= totalWorker {
+
+			//here we control the workers quantity and keep them below/equal the configured value on config.json file.
+			//if all x workers are busy, we wait for all of them to finish their job and then continue the loop.
+			if int64(busyWorkers) >= totalWorkers {
 				log.Println(("Waiting for idle workers..."))
 				wg.Wait()
 				busyWorkers = 0
 			}
+
 			wg.Add(1)
 			busyWorkers += 1
+
+			//query the data for this node.
 			go retriever.NodeQuery(&busyWorkers, &wg, nedata, errdata, cmds, ne, Config)
 		}
+
+		//all nodes are queried. we wait for all remaining workers to return their result.
 		wg.Wait()
+
+		//setting all workers to idle state.
 		busyWorkers = 0
+
+		//this for collects the result of each node from the nedata channel.
+		//if a valid data is collected from the node, it sends the data to UtilizationAssessment() to be parsed.
+		//if errdata is true, the query has failed for that node and it will be skipped.
+		//################################################################################################################################################
 		for _, m := range Nodes {
 			select {
 			case <-errdata:
@@ -55,7 +108,10 @@ func main() {
 				retriever.UtilizationAssessment(Nodes, result)
 			}
 		}
-		log.Println("going for sleep...")
+		//################################################################################################################################################
+		//resting as specified in the config file.
+		log.Printf("Resting for %v seconds", qint)
+
 		time.Sleep(time.Duration(qint) * time.Second)
 	}
 }
